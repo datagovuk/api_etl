826ccaeab8e0a4e2c46aa33d13ed569d8f9e7bca
Ross Jones <ross@servercode.co.uk>
2015-08-14 15:44:39 +0100

    Fix broken gitignore and add missing lib folder

---
+++ api_etl/lib/__init__.py
@@ -0,0 +1,3 @@
+from .extractor import Extractor
+from .transformer import Transformer
+from .loader import Loader
---
+++ api_etl/lib/services.py
@@ -0,0 +1,27 @@
+from pkg_resources import iter_entry_points
+
+
+def service_names():
+    """
+    Returns the name of all of the known services
+    """
+    return [ep.name for ep in iter_entry_points(group='services', name=None)]
+
+def service(name):
+    """
+    Returns the list of ETL object dicts for the specified service
+    """
+    entry_point = list(iter_entry_points(group='services', name=name))
+    if not entry_point:
+        raise ImportError("Can't load the {} endpoints".format(name))
+    endpoint_func = entry_point[0].load()
+    return endpoint_func()
+
+def named_subservice(service_name, subservice):
+    """ Returns subservice entrypoints from a top level service, such as
+    'health' and 'hospital' """
+    eps = service(service_name)
+    for ep in eps:
+        if ep['name'] == subservice:
+            return ep
+    raise ImportError("Could not find '{}' subservice in '{}'".format(subservice, service_name))
