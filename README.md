7fd14ff486f877d615c9982e913f06a8b15e2f34
Ross Jones <ross@servercode.co.uk>
2015-08-17 12:38:43 +0100

    Updated readme and database setup

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # ETL Tools for DGU API Server
 
-This repository contains a collection of tools which will, based on a YAML manifest file:
+This repository contains a collection of tools which will, based on a YAML manifest files:
 
+* Create a database for a theme 
 * Download the RAW data via data.gov.uk
 * Transforms the data in some small ways (e.g. split postcode to store outward code separately)
 * Create the necessary database and database tables, and grant read-only permission to the default user 
@@ -16,7 +17,32 @@ APIs are split into themes, and services, where each theme can have multiple ser
 
 ## Configuration
 
+### First task 
 
+Before setting the configuration file, the following command should be run to obtain the command line required to set up the accounts.  You will be prompted for the password for each user, which will be used in the configuration file later.
+
+```
+etl database once 
+```
+
+### Configuration file 
+
+Copy the sample.ini file and change the settings to reflect your setup.  Once the new .ini file is complete, the full path to the file should be set as an environment variable:
+
+```
+export DGU_ETL_CONFIG='/var/etl/config.ini'
+```
+
+Each command that requires config fill complain if this is not set as it will not be able to locate the configuration file.
+
+
+### Database 
+
+Databases for specific themes can (and should) be created with the ```etl database``` command which will create the database and set the permissions for the read-only user.
+
+```
+etl database init <theme> 
+```
 
 
 ## Running ETL
--- api_etl/commands/etl.py
+++ api_etl/commands/etl.py
@@ -5,9 +5,30 @@ import sys
 import api_etl.lib.services as svcs
 from api_etl.lib.manifest import Manifest
 
-class NullCommand(cmd.Cmd):
-    def default(self, *args):
-        print >> sys.stderr, "Unknown command"
+class DbCommand(cmd.Cmd):
+
+    def do_once(self, args):
+        """ Creates the necessary roles, a super user and a reader account """
+        accs = [
+            "createuser -s ckan",
+            "createuser -s -D -R -P -l reader",
+        ]
+        print ';\n'.join(accs) + ";"
+
+    def do_init(self, args):
+        """ Create the database for the '''args''' theme """
+        cmds = [
+            'createdb {db} -O {owner} -E utf-8'.format(db=args, owner="ckan"),
+            'psql {db} -c "GRANT CONNECT ON DATABASE {db} TO {role}"',
+            'psql {db} -c "GRANT USAGE ON SCHEMA public TO {role}"',
+            'psql {db} -c "GRANT SELECT ON ALL TABLES IN SCHEMA public TO {role}"',
+            'psql {db} -c "GRANT SELECT ON ALL SEQUENCES IN SCHEMA public TO {role}"',
+            'psql {db} -c "ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO {role}"',
+            'psql {db} -c "ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON SEQUENCES TO {role}"',
+            'psql {db} -c "REVOKE CREATE ON SCHEMA public FROM public"'
+        ]
+        print ';\n'.join(cmds).format(db=args,ro="ckan_reader", role='bob') + ";"
+
 
 class ServiceCommand(cmd.Cmd):
 
@@ -26,24 +47,53 @@ class ServiceCommand(cmd.Cmd):
             services = [svcs.named_subservice(*parts)]
             service_entries = [manifest.get_service(parts[1])]
 
-        for s in service_entries:
-            self.run_etl(s)
+        entry = zip(services, service_entries)
+        for e in entry:
+            ep, sm = e
+            self.run_etl(ep, sm)
 
-    def run_etl(self, service):
+    def print_separator(self, c='*'):
+        print c * 60
+
+
+    def run_etl(self, entry_points, service_manifest):
         """ Runs ETL by finding the manifest for each service based on the name"""
-        print "Running ETL with {} service".format(service.name)
+        self.print_separator('=')
+        print "Running ETL with {} service".format(service_manifest.name)
+        self.print_separator('=')
+
+        extractor = entry_points['extractor']()
+        print "\nExtracting data"
+        self.print_separator()
 
 
+        transformer = entry_points['transformer']()
+        print "\nTransforming data"
+        self.print_separator()
+
+
+        loader = entry_points['loader']()
+        print "\nLoading data"
+        self.print_separator()
 
 
 def main():
     commands = {
-        'service': ServiceCommand
+        'service': ServiceCommand,
+        'database': DbCommand
     }
 
     args = sys.argv[1:]
+    if not args:
+        print >> sys.stderr, "Argument are required"
+        sys.exit(1)
+
     command = args.pop(0)
     subcommand = args.pop(0) if args else ''
 
-    c = commands.get(command, NullCommand)()
+    if not command in commands:
+        print >> sys.stderr, "Unknown command"
+        sys.exit(1)
+
+    c = commands.get(command)()
     c.onecmd("{} {}".format(subcommand, ' '.join(args)))
